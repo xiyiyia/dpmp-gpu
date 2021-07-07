c4e28d83c6f4a833a2a75aded468502fc2923c64
xiyiyia <fuboqian@cug.edu.cn>
2021-07-07 13:03:04 +0900

    a

--- ps_pytorch-master/tools/local_script.sh
+++ ps_pytorch-master/tools/local_script.sh
@@ -7,4 +7,4 @@ ssh -o "StrictHostKeyChecking no" root@${PUB_IP_ADDR} -p 6003
 scp -i ${KEY_PEM_DIR} ${KEY_PEM_DIR} root@${PUB_IP_ADDR}:~/.ssh
 scp -i ${KEY_PEM_DIR} hosts hosts_address config root@${PUB_IP_ADDR}:~/
 scp -i ${KEY_PEM_DIR} -r /home/dpmp/dpmp-gpu/ps_pytorch-master root@${PUB_IP_ADDR}:~/
-ssh -i ${KEY_PEM_DIR} root@${PUB_IP_ADDR} 'cp /home/dpmp/dpmp-gpu/ps_pytorch-master/tools/remote_script.sh ~/; bash /home/dpmp/dpmp-gpu/ps_pytorch-master/tools/conda_install.sh'
+ssh -i ${KEY_PEM_DIR} root@${PUB_IP_ADDR} 'cp /home/dpmp/dpmp-gpu/ps_pytorch-master/tools/remote_script.sh ~/'
