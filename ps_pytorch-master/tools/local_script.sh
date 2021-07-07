aa249ecd69874c846492d7d4af79bf842c745d22
xiyiyia <fuboqian@cug.edu.cn>
2021-07-07 13:10:35 +0900

    port_error

--- ps_pytorch-master/tools/local_script.sh
+++ ps_pytorch-master/tools/local_script.sh
@@ -3,7 +3,7 @@ KEY_PEM_NAME=id_rsa.pub
 PUB_IP_ADDR="$1"
 echo "Public address of master node: ${PUB_IP_ADDR}"
 
-ssh -o "StrictHostKeyChecking no" root@${PUB_IP_ADDR} -p 6003
+ssh -o "StrictHostKeyChecking no" root@${PUB_IP_ADDR}
 scp -i ${KEY_PEM_DIR} ${KEY_PEM_DIR} root@${PUB_IP_ADDR}:~/.ssh
 scp -i ${KEY_PEM_DIR} hosts hosts_address config root@${PUB_IP_ADDR}:~/
 scp -i ${KEY_PEM_DIR} -r /home/dpmp/dpmp-gpu/ps_pytorch-master root@${PUB_IP_ADDR}:~/
