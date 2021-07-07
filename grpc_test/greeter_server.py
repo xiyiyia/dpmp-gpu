d203b003f321db1a40e0ad6fbb3097ad224f3c51
xiyiyia <fuboqian@cug.edu.cn>
2021-07-07 22:21:10 +0900

    port

--- grpc_test/greeter_server.py
+++ grpc_test/greeter_server.py
@@ -31,7 +31,7 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port(':6003')
+    server.add_insecure_port('[::]:6003')
     server.start()
     server.wait_for_termination()
 
