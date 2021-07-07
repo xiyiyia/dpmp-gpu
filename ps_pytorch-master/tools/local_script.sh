dd937338e783e030a444f39896b2b004a94b389d
xiyiyia <fuboqian@cug.edu.cn>
2021-07-07 12:40:39 +0900

    pem_ps_intl

--- ps_pytorch-master/tools/local_script.sh
+++ ps_pytorch-master/tools/local_script.sh
@@ -3,8 +3,8 @@ KEY_PEM_NAME=1.pem
 PUB_IP_ADDR="$1"
 echo "Public address of master node: ${PUB_IP_ADDR}"
 
-ssh -o "StrictHostKeyChecking no" ubuntu@${PUB_IP_ADDR}
-scp -i ${KEY_PEM_DIR} ${KEY_PEM_DIR} ubuntu@${PUB_IP_ADDR}:~/.ssh
-scp -i ${KEY_PEM_DIR} hosts hosts_address config ubuntu@${PUB_IP_ADDR}:~/
-scp -i ${KEY_PEM_DIR} -r /home/hwang/My_Code/ps_pytorch ubuntu@${PUB_IP_ADDR}:~/
-ssh -i ${KEY_PEM_DIR} ubuntu@${PUB_IP_ADDR} 'cp ps_pytorch/tools/remote_script.sh ~/; bash ~/ps_pytorch/tools/conda_install.sh; cp /home/ubuntu/grad_lossy_compression/tools/killall.sh ~/'
+ssh -o "StrictHostKeyChecking no" root@${PUB_IP_ADDR}
+scp -i ${KEY_PEM_DIR} ${KEY_PEM_DIR} root@${PUB_IP_ADDR}:~/.ssh
+scp -i ${KEY_PEM_DIR} hosts hosts_address config root@${PUB_IP_ADDR}:~/
+scp -i ${KEY_PEM_DIR} -r /home/dpmp/dpmp-gpu/ps_pytorch-master root@${PUB_IP_ADDR}:~/
+ssh -i ${KEY_PEM_DIR} root@${PUB_IP_ADDR} 'cp /home/dpmp/dpmp-gpu/ps_pytorch-master/tools/remote_script.sh ~/; bash /home/dpmp/dpmp-gpu/ps_pytorch-master/tools/conda_install.sh'
