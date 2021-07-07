62ea75d5082936d9bb6d2ded88dc5acf749600ab
xiyiyia <fuboqian@cug.edu.cn>
2021-07-07 22:52:07 +0900

    a

--- grpc_test/client.py
+++ grpc_test/client.py
@@ -9,7 +9,7 @@ _PORT = '6003'
 def run():
     conn = grpc.insecure_channel(_HOST + ':' + _PORT)
     client = msg_pb2_grpc.MsgServiceStub(channel=conn)
-    response = client.GetMsg(msg_pb2.RequestData(text='hello,world!'))
+    response = client.GetMsg(msg_pb2.MsgRequest(text='hello,world!'))
     print("received: " + response.text)
 
 if __name__ == '__main__':
