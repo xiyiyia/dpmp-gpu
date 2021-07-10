99595b0247bce3e25d201b41a750a5854dbd90ef
xiyiyia <fuboqian@cug.edu.cn>
2021-07-10 16:56:44 +0900

    a

--- dpmp_one_machine/test/test_mp.py
+++ dpmp_one_machine/test/test_mp.py
@@ -68,7 +68,8 @@ def partition_dataset():
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.1307,), (0.3081,))
                              ]))
-    size = dist.get_world_size()
+    # size = dist.get_world_size()
+    size = 1
     bsz = 128 / float(size)
     partition_sizes = [1.0 / size for _ in range(size)]
     partition = DataPartitioner(dataset, partition_sizes)
