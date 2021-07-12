dc8b8b5a51ff1f2e2fb9510c8d2938fcd19358eb
xiyiyia <fuboqian@cug.edu.cn>
2021-07-12 22:57:16 +0900

    a

--- dpmp_one_machine/test/models/resnet_gpu.py
+++ dpmp_one_machine/test/models/resnet_gpu.py
@@ -179,12 +179,13 @@ class ResNet(nn.Module):
         ret = []
         for s_next in splits:
             # print(s_next, output_list)
-            s_prev = self.feature_list[0](s_next).to('cuda:1')
+            s_prev = self.feature_list[0](s_next)
             output_list[0] = s_prev.to('cuda:1')
             # print(output_list)
             for j in range(len(output_list) - 1):
                 # print(output_list)
                 if(output_list[len(output_list) - j - 1] != None):
+                    print(j,output_list[len(output_list) - j - 1],'no None')
                     if(j == 0):
                         ret.append(self.classifier(output_list[len(output_list) - j - 1].view(s_prev.size()[0], -1)))
                         output_list[len(output_list) - j - 1] = None
