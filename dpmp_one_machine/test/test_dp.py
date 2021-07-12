85753f54773187f932acf1f07fcb086407be15e1
xiyiyia <fuboqian@cug.edu.cn>
2021-07-13 02:38:08 +0900

    a

--- dpmp_one_machine/test/test_dp.py
+++ dpmp_one_machine/test/test_dp.py
@@ -289,8 +289,8 @@ if __name__ == "__main__":
     parser.add_argument('-g', type=int, default=1, help='number of gpus')
     args = parser.parse_args()
     # print(torch.cuda.device_count())
-    # size = args.g
-    size = torch.cuda.device_count()
+    size = args.g
+    # size = torch.cuda.device_count()
     processes = []
     mp.set_start_method("spawn")
     
