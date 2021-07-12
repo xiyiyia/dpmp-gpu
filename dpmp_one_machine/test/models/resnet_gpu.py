7d02c33af49597bbc01def0f72a2f36f2163ed02
xiyiyia <fuboqian@cug.edu.cn>
2021-07-12 23:00:35 +0900

    a

--- dpmp_one_machine/test/models/resnet_gpu.py
+++ dpmp_one_machine/test/models/resnet_gpu.py
@@ -188,7 +188,7 @@ class ResNet(nn.Module):
                 if(output_list[len(output_list) - j - 2] != None):
                     print(j,output_list[len(output_list) - j - 2],'no None')
                     if(j == 0):
-                        ret.append(self.classifier(output_list[len(output_list) - j - 2].view(s_prev.size()[0], -1)))
+                        ret.append(self.fc(output_list[len(output_list) - j - 2].view(s_prev.size()[0], -1)))
                         output_list[len(output_list) - j - 2] = None
                     else:
                         output_list[len(output_list) - j - 1] = self.feature_list[len(output_list) - j - 1](output_list[len(output_list) - j - 2]).to('cuda:' + str(len(output_list) - j -1 ))
@@ -199,7 +199,7 @@ class ResNet(nn.Module):
             for j in range(len(output_list) - 1):
                 if(output_list[len(output_list) - j - 1] != None):
                     if(j == 0):
-                        ret.append(self.classifier(output_list[len(output_list) - j - 2].view(s_prev.size()[0], -1)))
+                        ret.append(self.fc(output_list[len(output_list) - j - 2].view(s_prev.size()[0], -1)))
                         output_list[len(output_list) - j - 2] = None
                     else:
                         output_list[len(output_list) - j - 1] = self.feature_list[len(output_list) - j - 1](output_list[len(output_list) - j - 2]).to('cuda:' + str(len(output_list) - j - 1))
