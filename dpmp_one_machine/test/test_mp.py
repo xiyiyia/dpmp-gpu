5548742e0f26e04dabf658df1c4743e7c470ed6f
xiyiyia <fuboqian@cug.edu.cn>
2021-07-12 23:56:01 +0900

    a

--- dpmp_one_machine/test/test_mp.py
+++ dpmp_one_machine/test/test_mp.py
@@ -181,8 +181,8 @@ if __name__ == "__main__":
     #model parallel compare 
     stmt = "run(model)"
 
-    setup = "model = ModelParallelvgg(g = 2)"
-    # setup = "model = resnet_gpu.resnet50(args)"
+    # setup = "model = ModelParallelvgg(g = 2)"
+    setup = "model = resnet_gpu.resnet50(args)"
     mp_run_times = timeit.repeat(
         stmt, setup, number=1, repeat=1, globals=globals())
     mp_mean, mp_std = np.mean(mp_run_times), np.std(mp_run_times)
