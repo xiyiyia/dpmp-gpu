4e65e7e280d4ba9a47deacc9e00bc3a334e163c2
xiyiyia <fuboqian@cug.edu.cn>
2021-07-13 14:23:39 +0900

    a

--- dpmp_one_machine/test/test_dp.py
+++ dpmp_one_machine/test/test_dp.py
@@ -247,7 +247,8 @@ def run(args, rank, size, model):
         print('Rank ', dist.get_rank(), ', epoch ',
                 epoch, ': ', epoch_loss / bsz)
     stop = time.time()
-    print('training_time_dp', stop - start)
+    if(rank == 0):
+        print('training_time_dp', stop - start)
     # test.to_csv('./testcsv.csv',encoding='gbk')
     # fl_c = open('./communication_time_'+str(dist.get_rank())+'.csv',"w+")
     # fl_c.write(training_time_list)
