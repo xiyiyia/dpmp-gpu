a8c4f04905cfb27d9b002e439ff7564056e95114
xiyiyia <fuboqian@cug.edu.cn>
2021-07-13 15:12:26 +0900

    a

--- dpmp_one_machine/test/test_mp.py
+++ dpmp_one_machine/test/test_mp.py
@@ -144,7 +144,7 @@ def run(args, model):
                           lr=0.01, momentum=0.5)
     # num_batches = math.ceil(len(train_set.dataset) / float(bsz))
     start = time.time()
-    for epoch in range(1):
+    for epoch in range(100):
         epoch_loss = 0.0
         for data, target in train_set:
             
