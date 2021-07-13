014cbeaace9bcf1da7b92b5c263afbae6c7a08f9
xiyiyia <fuboqian@cug.edu.cn>
2021-07-13 16:36:38 +0900

    a

--- dpmp_one_machine/test/models/resnet_gpu.py
+++ dpmp_one_machine/test/models/resnet_gpu.py
@@ -10,7 +10,7 @@
 
 import torch
 import torch.nn as nn
-
+import time
 class BasicBlock(nn.Module):
     """Basic Block for resnet 18 and resnet 34
 
@@ -203,6 +203,7 @@ class ResNet(nn.Module):
         # output = output.view(output.size(0), -1)
         # output = self.fc(output)
         # i = 1
+        start = time.time()
         output_list = [None for i in range(self.g)]
         splits = iter(x.split(self.split_size, dim=0))
         s_next = next(splits)
@@ -239,7 +240,8 @@ class ResNet(nn.Module):
                     break
         # print(len(torch.cat(ret)[0]))
         # print(ret)
-
+        stop = time.time()
+        print("real_training_time", stop - start)
         return torch.cat(ret)
 
 def resnet18(args):
