a11c818ce5ff1fc0cddb0666a0dccaabb31a2adf
xiyiyia <fuboqian@cug.edu.cn>
2021-07-14 12:40:22 +0900

    a

--- dpmp_one_machine/test/test_dp.py
+++ dpmp_one_machine/test/test_dp.py
@@ -72,7 +72,7 @@ def partition_dataset(args):
     partition = DataPartitioner(dataset, partition_sizes)
     partition = partition.use(dist.get_rank())
     train_set = torch.utils.data.DataLoader(partition,
-                                         batch_size=bsz/args.g,
+                                         batch_size=int(bsz/args.g),
                                          shuffle=True)
     #print(train_set, bsz)
     return train_set, bsz
