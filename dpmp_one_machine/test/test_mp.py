2c9e6e5310416ad4d6f1d087432a2cd3624c4ce6
xiyiyia <fuboqian@cug.edu.cn>
2021-07-14 09:33:11 +0900

    a

--- dpmp_one_machine/test/test_mp.py
+++ dpmp_one_machine/test/test_mp.py
@@ -230,12 +230,12 @@ def run(args, model):
     # print(sum(1 for _ in resnet152()))
     
     
-    # partitions = torch.cuda.device_count()
-    # partitions = args.g
-    # sample = torch.empty(args.b, 3, 224, 224).cuda()
-    # balance = balance_by_time(partitions, resnet152(), sample, device=torch.device('cuda'))
-    # model = GPipe(resnet152(), balance, chunks=10)
-    print(model)
+    partitions = torch.cuda.device_count()
+    partitions = args.g
+    sample = torch.empty(args.b, 3, 224, 224).cuda()
+    balance = balance_by_time(partitions, resnet152(), sample, device=torch.device('cuda'))
+    model = GPipe(resnet152(), balance, chunks=10)
+    # print(model)
     # summary(model.cuda(), [(3, 255, 255)])
     dataset = torchvision.datasets.CIFAR10('./data', train=True, download=True,
                              transform=transforms.Compose([
