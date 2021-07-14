d46eea5dbb98ce2620e03a907e533f54e2f5e5e2
xiyiyia <fuboqian@cug.edu.cn>
2021-07-14 09:06:31 +0900

    a

--- dpmp_one_machine/test/test_mp.py
+++ dpmp_one_machine/test/test_mp.py
@@ -231,10 +231,10 @@ def run(args, model):
     
     
     # partitions = torch.cuda.device_count()
-    partitions = args.g
-    sample = torch.empty(args.b, 3, 224, 224).cuda()
-    balance = balance_by_time(partitions, resnet152(), sample, device=torch.device('cuda'))
-    model = GPipe(resnet152(), balance, chunks=10)
+    # partitions = args.g
+    # sample = torch.empty(args.b, 3, 224, 224).cuda()
+    # balance = balance_by_time(partitions, resnet152(), sample, device=torch.device('cuda'))
+    # model = GPipe(resnet152(), balance, chunks=10)
     print(model)
     # summary(model.cuda(), [(3, 255, 255)])
     dataset = torchvision.datasets.CIFAR10('./data', train=True, download=True,
@@ -304,8 +304,8 @@ if __name__ == "__main__":
     #model parallel compare 
     stmt = "run(args,model)"
 
-    # setup = "model = ModelParallelvgg(g = 2)"
-    setup = "model = resnet_gpu.resnet152(args)"
+    setup = "model = ModelParallelvgg(g = 2)"
+    # setup = "model = resnet_gpu.resnet152(args)"
     # setup = "model = resnet.resnet152()"
     # setup = "model = resnet_gpipe.resnet152(args)"
     # setup = "model = resnet152(args)"
