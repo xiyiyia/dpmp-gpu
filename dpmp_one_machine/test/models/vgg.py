f4775be50350e7db4faebf64ea4a2d109f361f0d
xiyiyia <fuboqian@cug.edu.cn>
2021-07-15 23:16:50 +0900

    a

--- dpmp_one_machine/test/models/vgg.py
+++ dpmp_one_machine/test/models/vgg.py
@@ -40,11 +40,11 @@ class VGG(nn.Module):
         #print(output.shape)
         output = output.view(output.size(0), -1)
         #print(output.shape)
-        print(output.shape)
+        # print(output.shape)
         # output = torch.randn(128, 512).cuda()
         # output = torch.randn(128, 128).cuda()
         output = self.classifier(output)
-        print(output.shape)
+        # print(output.shape)
 
         return output
 
