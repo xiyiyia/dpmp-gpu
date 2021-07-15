a466c0c109a97064d6a954da307c709f870acdf3
xiyiyia <fuboqian@cug.edu.cn>
2021-07-15 23:18:33 +0900

    a

--- dpmp_one_machine/test/test_dp.py
+++ dpmp_one_machine/test/test_dp.py
@@ -174,7 +174,7 @@ def run(rank, size, model, epochs, args, data):
                 optimizer.step()
                 optimizer.zero_grad()
             if(rank == 0):
-                # print("why")
+                print("print")
                 percent = (i+1) / len(data) * 100
                 throughput = data_trained / (time.time()-tick)
                 log('%d/%d epoch (%d%%) | %.3f samples/sec (estimated)'
