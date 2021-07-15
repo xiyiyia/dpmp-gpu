fcc171c000eaaf1268ba6acbcb50b4f23b669de6
xiyiyia <fuboqian@cug.edu.cn>
2021-07-15 16:48:13 +0900

    a

--- dpmp_one_machine/test/test_dp.py
+++ dpmp_one_machine/test/test_dp.py
@@ -215,8 +215,8 @@ def init_process(args,rank, fn, backend='gloo'):
     input = torch.rand(args.b, 3, 224, 224, device='cuda:'+str(rank))
     target = torch.randint(10, (args.b,), device='cuda:'+str(rank))
     data = [(input, target)] * (dataset_size//args.b)
-    print(dataset_size)
-    print(data[0])
+    # print(dataset_size)
+    # print(data[0])
 
     fn(rank, args.g, model, data, args.e)
 
