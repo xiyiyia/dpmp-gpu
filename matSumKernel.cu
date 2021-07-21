9eadf2bc2f1542937c50774c1bf68eaa7949ccdd
xiyiyia <fuboqian@cug.edu.cn>
2021-07-21 01:11:28 +0000

    a

---
+++ matSumKernel.cu
@@ -0,0 +1,10 @@
+#include <cmath>
+using namespace std;
+
+__global__ void hello_from_gpu()
+{
+    const int b = blockIdx.x;
+    const int tx = threadIdx.x;
+    const int ty = threadIdx.y;
+    printf("Hello World from block-%d and thread-(%d, %d)!\n", b, tx, ty);
+}
