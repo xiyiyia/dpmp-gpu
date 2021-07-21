79a867e5d662b3f1a38a25d46942f090779c691f
xiyiyia <fuboqian@cug.edu.cn>
2021-07-21 10:27:01 +0900

    a

--- matSumKernel.cu
+++ matSumKernel.cu
@@ -3,7 +3,8 @@ using namespace std;
 
 __global__ void hello_from_gpu()
 {
-    const int b = blockIdx.x;
-    const int tx = threadIdx.x;
-    const int ty = threadIdx.y;
+    const int b = 0;
+    //const int tx = threadIdx.x;
+    //const int ty = threadIdx.y;
+    //b 
 }
