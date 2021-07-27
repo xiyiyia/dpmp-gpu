e59f88c5032d0c849288fc5602ea9c3e74447002
xiyiyia <fuboqian@cug.edu.cn>
2021-07-27 22:12:39 +0900

    a

--- ml-project/mnist_example.py
+++ ml-project/mnist_example.py
@@ -162,11 +162,11 @@ for i, (batch, labels) in enumerate(train_loader):
     train_features[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = rbm.sample_hidden(batch).cpu().numpy()
     train_labels[i*BATCH_SIZE:i*BATCH_SIZE+len(batch)] = labels.numpy()
 
-print(len(train_labels[0]))
+# print(len(train_labels[0]))
 plt.figure(figsize=(5,5), dpi=180)
 for i in range(0,4):
     for j in range(0,4):
-        img = train_features[0][i*4+j].reshape(28,28)
+        img = train_features[(i*4+j)*BATCH_SIZE:(i*4+j+1)*BATCH_SIZE].reshape(28,28)
         plt.subplot(4,4,i*4+j+1)
         plt.imshow(img ,cmap = plt.cm.gray)
 plt.savefig('./pic/'+args.d+'.png')
