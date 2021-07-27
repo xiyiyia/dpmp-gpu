2596037de9fb7e9081aac4e0a8df70747e16d3c8
xiyiyia <fuboqian@cug.edu.cn>
2021-07-27 16:50:08 +0900

    a

--- ml-project/RBM.py
+++ ml-project/RBM.py
@@ -286,6 +286,7 @@ def test_rbm(args,k=1):
     rbm.fit(data,epochs=args.e)
     rebuild_value = []
     for i in test:
+        print(i)
         rebuild_value.append([rbm.predict(i)])
         break
     visualize(args,rebuild_value)
